a096639173518d6c3bd4011e4da64bc6bac854e8
Noor231 <hsingh20@ilg.cat>
2022-10-10 20:43:19 +0200

    commit before revert

--- Practica1/Program.java
+++ Practica1/Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2022 Noor singh <hsingh20@ilg.cat>
+ * Copyright 2022 Joan seculi <jseculiescoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
